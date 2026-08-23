e1425a87e02a635b503aa68f6967d6cb5e230977
HuynhDang <nguyendangchanell@gmail.com>
2026-08-23 10:04:34 +0700

    feat: update common button and text_field

--- lib/src/core/presentation/widgets/text_field.dart
+++ lib/src/core/presentation/widgets/text_field.dart
@@ -1,110 +1,193 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
-import 'package:healthlife/generated/assets.gen.dart';
-import 'package:healthlife/src/common/constants/colors.dart';
 import 'package:healthlife/src/common/extensions/color_extension.dart';
+import 'package:healthlife/src/core/presentation/widgets/button.dart';
 
-class AppTextField extends StatefulWidget {
-  const AppTextField({
+import '../../../../generated/assets.gen.dart';
+import '../../../../generated/fonts.gen.dart';
+import '../../../common/constants/colors.dart';
+import 'text.dart';
+
+class AppTF extends Column {
+  AppTF.common({
     super.key,
-    this.controller,
-    this.hintText,
-    this.labelText,
-    this.obscureText = false,
-    this.keyboardType,
-    this.prefixIcon,
-    this.suffixIcon,
-    this.validator,
-    this.onChanged,
-    this.onSubmitted,
-    this.enabled = true,
-    this.maxLines = 1,
-    this.textInputAction,
-  });
+    required TextEditingController controller,
+    String? label,
+    String? hintText,
+    TextInputType? keyboardType,
+    Function(String)? onChanged,
+    Function(String)? onSubmitted,
+    bool showInfo = false,
+    VoidCallback? onTapInfo,
+    bool autofocus = false,
+    FocusNode? focusNode,
+    Widget? leftWidget,
+    Widget? rightWidget,
+    Color? bgColor,
+    Color? textColor,
+    double? height,
+  }) : super(
+         crossAxisAlignment: CrossAxisAlignment.start,
+         children: [
+           Container(
+             height: height ?? 46,
+             padding: const EdgeInsets.symmetric(horizontal: 12),
+             decoration: BoxDecoration(
+               color: bgColor,
+               border: bgColor != null
+                   ? null
+                   : Border.all(color: UIColors.separate),
+               borderRadius: BorderRadius.circular(12),
+             ),
+             alignment: Alignment.centerLeft,
+             child: Row(
+               children: [
+                 if (leftWidget != null)
+                   Padding(
+                     padding: const EdgeInsets.only(right: 12),
+                     child: leftWidget,
+                   ),
+                 Expanded(
+                   child: TextField(
+                     cursorWidth: 1,
+                     autofocus: autofocus,
+                     cursorHeight: 16,
+                     cursorColor: textColor ?? UIColors.text,
+                     controller: controller,
+                     focusNode: focusNode,
+                     decoration: InputDecoration(
+                       contentPadding: const EdgeInsets.only(bottom: 2),
+                       hintText: (hintText != null) ? tr(hintText) : null,
+                       hintStyle: TextStyle(
+                         color: textColor?.withAlpha(100) ?? UIColors.textBody,
+                         fontSize: 14,
+                         fontFamily: FontFamily.inter,
+                       ),
+                       border: InputBorder.none,
+                     ),
+                     autocorrect: false,
+                     style: TextStyle(
+                       color: textColor ?? UIColors.text,
+                       fontWeight: FontWeight.w500,
+                       fontSize: 14,
+                       fontFamily: FontFamily.inter,
+                     ),
+                     textInputAction: TextInputAction.done,
+                     keyboardType: keyboardType,
+                     onChanged: onChanged,
+                     onSubmitted: onSubmitted,
+                     keyboardAppearance: Brightness.dark,
+                   ),
+                 ),
+                 if (rightWidget != null)
+                   Padding(
+                     padding: const EdgeInsets.only(left: 12),
+                     child: rightWidget,
+                   ),
+               ],
+             ),
+           ),
+         ],
+       );
 
-  final TextEditingController? controller;
-  final String? hintText;
-  final String? labelText;
-  final bool obscureText;
-  final TextInputType? keyboardType;
-  final Widget? prefixIcon;
-  final Widget? suffixIcon;
-  final String? Function(String?)? validator;
-  final ValueChanged<String>? onChanged;
-  final ValueChanged<String>? onSubmitted;
-  final bool enabled;
-  final int maxLines;
-  final TextInputAction? textInputAction;
+  AppTF.password({
+    super.key,
+    required TextEditingController controller,
+    String? label,
+    String? hintText,
+    TextInputType? keyboardType,
+    Function(String)? onChanged,
+    Function(String)? onSubmitted,
+    bool showPassword = false,
+    VoidCallback? onChangeShowPassword,
+    Color? bgColor,
+    Color? textColor,
+  }) : super(
+         crossAxisAlignment: CrossAxisAlignment.start,
+         children: [
+           Container(
+             height: 46,
+             padding: const EdgeInsets.symmetric(horizontal: 16),
+             decoration: BoxDecoration(
+               color: bgColor,
+               border: bgColor != null
+                   ? null
+                   : Border.all(color: UIColors.separate),
+               borderRadius: BorderRadius.circular(12),
+             ),
+             alignment: Alignment.centerLeft,
+             child: Row(
+               children: [
+                 Expanded(
+                   child: TextField(
+                     cursorWidth: 1,
+                     autofocus: false,
+                     obscureText: !showPassword,
+                     cursorHeight: 16,
+                     cursorColor: textColor ?? UIColors.text,
+                     controller: controller,
+                     decoration: InputDecoration(
+                       contentPadding: const EdgeInsets.only(bottom: 2),
+                       hintText: (hintText != null) ? tr(hintText) : null,
+                       hintStyle: TextStyle(
+                         color: (textColor ?? UIColors.text).withAlpha(100),
+                         fontSize: 14,
+                         fontFamily: FontFamily.inter,
+                         fontWeight: FontWeight.normal,
+                       ),
+                       border: InputBorder.none,
+                     ),
+                     autocorrect: false,
+                     style: TextStyle(
+                       color: textColor ?? UIColors.text,
+                       fontWeight: FontWeight.w500,
+                       fontSize: 14,
+                       fontFamily: FontFamily.inter,
+                     ),
+                     textInputAction: TextInputAction.done,
+                     keyboardType: keyboardType,
+                     onChanged: onChanged,
+                     onSubmitted: onSubmitted,
+                     keyboardAppearance: Brightness.dark,
+                   ),
+                 ),
+                 AppButton.widget(
+                   child: Padding(
+                     padding: const EdgeInsets.all(8),
+                     child:
+                         (showPassword
+                                 ? Assets.svg.icEyeOn
+                                 : Assets.svg.icEyeOff)
+                             .svg(
+                               colorFilter: (textColor ?? UIColors.text).filter,
+                             ),
+                   ),
+                   onTap: () {
+                     if (onChangeShowPassword != null) {
+                       onChangeShowPassword();
+                     }
+                   },
+                 ),
+               ],
+             ),
+           ),
+         ],
+       );
 
-  @override
-  State<AppTextField> createState() => _AppTextFieldState();
-}
-
-class _AppTextFieldState extends State<AppTextField> {
-  late bool _obscured = widget.obscureText;
-
-  OutlineInputBorder _border(Color color, {double width = 1}) =>
-      OutlineInputBorder(
-        borderRadius: BorderRadius.circular(12),
-        borderSide: BorderSide(color: color, width: width),
+  AppTF.disable({super.key, String? label, required String value})
+    : super(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            height: 46,
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            decoration: BoxDecoration(
+              color: UIColors.lightGray,
+              borderRadius: BorderRadius.circular(12),
+            ),
+            alignment: Alignment.centerLeft,
+            child: AppText.medium(value),
+          ),
+        ],
       );
-
-  @override
-  Widget build(BuildContext context) {
-    final isDark = Theme.of(context).brightness == Brightness.dark;
-    final textColor =
-        isDark ? UIColors.darkTextPrimary : UIColors.lightTextPrimary;
-    final secondaryColor =
-        isDark ? UIColors.darkTextSecondary : UIColors.lightTextSecondary;
-    final fillColor = isDark ? UIColors.darkSurface : UIColors.lightSurface;
-    final borderColor = isDark ? UIColors.darkBorder : UIColors.lightBorder;
-
-    return TextFormField(
-      controller: widget.controller,
-      obscureText: _obscured,
-      keyboardType: widget.keyboardType,
-      validator: widget.validator,
-      onChanged: widget.onChanged,
-      onFieldSubmitted: widget.onSubmitted,
-      enabled: widget.enabled,
-      maxLines: widget.obscureText ? 1 : widget.maxLines,
-      textInputAction: widget.textInputAction,
-      cursorColor: UIColors.pink,
-      style: TextStyle(color: textColor),
-      decoration: InputDecoration(
-        labelText: widget.labelText,
-        labelStyle: TextStyle(color: secondaryColor),
-        hintText: widget.hintText,
-        hintStyle: TextStyle(color: secondaryColor),
-        prefixIcon: widget.prefixIcon,
-        suffixIcon: widget.suffixIcon ?? _buildSuffix(),
-        filled: true,
-        fillColor: fillColor,
-        contentPadding:
-            const EdgeInsets.symmetric(horizontal: 16, vertical: 14),
-        border: _border(borderColor),
-        enabledBorder: _border(borderColor),
-        focusedBorder: _border(UIColors.pink, width: 1.5),
-        disabledBorder: _border(borderColor),
-        errorBorder: _border(UIColors.coral),
-        focusedErrorBorder: _border(UIColors.coral, width: 1.5),
-        errorStyle: const TextStyle(color: UIColors.coral),
-      ),
-    );
-  }
-
-  Widget? _buildSuffix() {
-    if (!widget.obscureText) return null;
-    final isDark = Theme.of(context).brightness == Brightness.dark;
-    final iconColor =
-        isDark ? UIColors.darkTextSecondary : UIColors.lightTextSecondary;
-
-    return IconButton(
-      onPressed: () => setState(() => _obscured = !_obscured),
-      icon: (_obscured ? Assets.svg.icEyeOff : Assets.svg.icEyeOn).svg(
-        width: 22,
-        height: 22,
-        colorFilter: iconColor.filter,
-      ),
-    );
-  }
 }
